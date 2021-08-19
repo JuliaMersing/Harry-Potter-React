dd7397ec5b371c465e9680cff29515b21291e18c
Julia Mersing <julia.mersing.ortiz@gmail.com>
2021-08-19 12:52:59 +0200

    add pages

--- src/components/NotFound/PageNotFound.js
+++ src/components/NotFound/PageNotFound.js
@@ -4,10 +4,10 @@ import "../../stylesheet/NotFound.scss";
 function PageNotFound() {
   return (
     <div className="notfound">
-      <p>Oh boy! You must be wrong, this page does not exist!</p>
+      <p>Keep studying your spells, this page does not exist!</p>
       <img
         className="notfound_img"
-        src="https://media3.giphy.com/media/AqOioh3rTS0Z3pP6V2/200.gif?cid=6104955e88bb50cc0c697bb25419b159b7d30129498cd934&rid=200.gif&ct=g&cid=6104955e88bb50cc0c697bb25419b159b7d30129498cd934&rid=200.gif&ct=g"
+        src="https://media0.giphy.com/media/BJmTtZL4hova8/200.gif?cid=6104955eff4406c0ad0178ba7fb6ff5896e440578c81972d&rid=200.gif&ct=g&cid=6104955eff4406c0ad0178ba7fb6ff5896e440578c81972d&rid=200.gif&ct=g"
         alt="This page doesn't exist"
       />
       <Link to="/">
